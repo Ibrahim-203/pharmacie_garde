576cb860a3f655d54f34813016368d55e1b78be3
Ibrahim <you@example.com>
2026-03-24 08:19:16 +0300

    Creating garde model.

---
+++ models/Garde.js
@@ -0,0 +1,30 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
+import Pharmacie from './Pharmacie.js';
+import Region from './Region.js';
+
+const Garde = sequelize.define('Garde', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  pharmacieId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  regionId:{
+    type : DataTypes.INTEGER,
+    allowNull: false,
+  },
+  dateDebut: DataTypes.DATE,
+  dateFin: DataTypes.DATE,
+}, {
+  tableName: 'garde',
+  timestamps: true,
+});
+
+Garde.belongsTo(Pharmacie, {foreignKey:'pharmacieId'})
+Garde.belongsTo(Region, {foreignKey:'regionId'})
+
+export default Garde;
